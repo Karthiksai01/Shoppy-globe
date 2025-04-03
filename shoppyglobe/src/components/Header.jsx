bea73fead33d89c80a72f561c1918a32dcca5732
Karthiksai01 <karthiksaibayya24@gmail.com>
2025-04-03 21:58:44 +0530

    file modified

--- shoppyglobe/src/components/Header.jsx
+++ shoppyglobe/src/components/Header.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { FaShoppingCart } from "react-icons/fa"; // Import cart icon
+import { FaShoppingCart } from "react-icons/fa"; 
 
 const Header = () => {
   const cartItems = useSelector(state => state.cart.cartItems);
-  const totalItems = cartItems.reduce((acc, item) => acc + item.quantity, 0); // Calculate total items
+  const totalItems = cartItems.reduce((acc, item) => acc + item.quantity, 0); 
 
   return (
-    //header component for displaying nav items
+  
     <header className="navItems">
       <h1>ShoppyGlobe</h1>
       <nav>
-        <Link to="/">Home</Link>   {/* it will redirect to home*/}
+        <Link to="/">Home</Link>   
         <Link to="/cart" className="cart-link">  {/* It is navigate to cart*/}
           <FaShoppingCart className="cart-icon" /> 
           {totalItems > 0 && <span className="cart-count">{totalItems}</span>}
